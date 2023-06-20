254155b2076577f805c935d204b7af31a0ef7847
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2023-06-20 15:30:53 +0300

    print_char

---
+++ print_char.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * pchar - print top
+ * @stack: ...
+ * @line_number: ...
+ */
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	stack_t *top = *stack;
+
+	if (!top)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		free_stack(global_data.stack);
+		fclose(global_data.file);
+		exit(EXIT_FAILURE);
+	}
+	if (top->n >= 0 && top->n <= 127)
+		printf("%c\n", top->n);
+	else
+	{
+		fprintf(stderr, "L%u: value out of range\n", line_number);
+		free_stack(global_data.stack);
+		free(global_data.line);
+		fclose(global_data.file);
+		exit(EXIT_FAILURE);
+	}
+}
