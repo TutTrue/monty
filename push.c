582b5dff0b18f05640c3cf205538dfb48181c365
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2023-06-23 02:32:24 +0300

    final

--- push.c
+++ push.c
@@ -18,8 +18,7 @@ void push(stack_t **stack, unsigned int line_number)
 		free(global_data.line);
 		exit(EXIT_FAILURE);
 	}
-	is_digits(global_data.str);
-	if (!atoi(global_data.str) && global_data.str[0] != '0')
+	if (!atoi(global_data.str) && global_data.str[0] != '0' && is_digits(global_data.str))
 	{
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		free_stack(global_data.stack);
